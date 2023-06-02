b27a466c807f0ae19d2f4fe0582303801c58f45b
Muserref Albayrak <albayrakmusereff@gmail.com>
2023-06-03 00:17:46 +0300

    3.06.2023

---
+++ day2.py
@@ -0,0 +1,30 @@
+faiz = 1.59;
+vade = 36;
+krediAdi = "Ihtiyac Kredisi"
+#type conversion
+print(type(faiz)) #degisken tipini ogrenmek istedigimde type() ile bunu yapabilmekteyim
+print(vade + int(faiz)) #faiz float data type idi int() icine aldim 
+print(str(faiz)); #string'e dondurmek icin str()
+vade = int(input("Lutfen istediginiz vade sayisini giriniz: "));
+#Kullanicidan alinan input default olarak String data type'tir. Python'in ozelligi.
+#ihtiyaca gore hangi data type olmaliysa input()'u istenilen data type'a donusturebilirsin.
+vade = vade + 12
+# String Interpolation
+#Kullanicinin girdigi deger sonucu elde edilen vade: 
+print("sectiginiz vade sonucu elde edilen vade : {vadeSayisi}".format(vadeSayisi=vade));
+print(type(f"sectiginiz vade sonucu elde edilen vade : {vade}"));
+#string concatination java mantiginda degil burada biraz. simdi; atring ifade icerisinde {} icerisine bir replika name tanimla
+#daha sonra o tanimlanan name'i sitring disinda .format() icerisinde istenen container'a esitle.
+
+#2nd way. f-string => yaygin olan
+name = "Muserref"
+text = f"Welcome {name}"
+print(text);
+
+x = 12
+y = "12.1"
+x = f"add result = {x}"
+print()
+
+print("Add result = {add}".format(add=(x)))
+print(type(x))
